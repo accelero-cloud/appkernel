47aa9571b79e39f11d718f65a2da9bfee88fc310
Csaba Tamas <tamas.csaba@gmail.com>
2018-07-24 21:21:17 +0200

    small adaptations to python 3.7

--- appkernel/service.py
+++ appkernel/service.py
@@ -261,9 +261,9 @@ class Service(RbacMixin):
                     model_instance = Model.from_dict(Service.__extract_dict_from_payload(), model_class)
                     # save or update the object
                     named_and_request_arguments.update(document=Model.to_dict(model_instance, convert_id=True))
+                    named_and_request_arguments.update(object=model_instance)
                     return_code = 201
                 elif request.method == 'PATCH':
-
                     named_and_request_arguments.update(document=Service.__extract_dict_from_payload())
                 result = provisioner_method(
                     **Service.__autobox_parameters(executable_method, named_and_request_arguments))
